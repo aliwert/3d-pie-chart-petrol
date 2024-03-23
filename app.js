6ab4cbbd381cf0e90b156d22aec5efd017e26a3e
aliwert <154356044+aliwert@users.noreply.github.com>
2024-03-23 11:25:22 +0300

    js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ chart.data = [
 //? Set inner radius (size of the hole in the middle) of the pie chart in percentage
 chart.innerRadius = am4core.percent(40);
 //! Set the depth (thickness) of the chart in pixels
+
 chart.depth = 120;
 
 //* Add a legend to the chart
@@ -45,11 +46,14 @@ chart.legend = new am4charts.Legend();
 
 //? Create a PieSeries3D for the chart
 let series = chart.series.push(new am4charts.PieSeries3D());
+
 //! Set data fields for the series
 series.dataFields.value = "litres";
 series.dataFields.depthValue = "litres";
 series.dataFields.category = "country";
+
 //! Set corner radius for the pie slices
 series.slices.template.cornerRadius = 6;
+
 //! Set color step for the series
 series.colors.step = 3;
